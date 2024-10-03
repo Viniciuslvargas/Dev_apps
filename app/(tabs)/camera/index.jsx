4f862ef6f478c46a93438fd37e27f2069fba84da
Viniciuslvargas <vinicius_l_vargas@estudante.sesisenai.org.br>
2024-10-03 11:31:11 -0300

    gg

--- app/(tabs)/camera/index.jsx
+++ app/(tabs)/camera/index.jsx
@@ -1,53 +1,103 @@
-import {useState, useRef} from 'react'
-import {View, StyleSheet, Text, Image, Button} from 'react-native'
-import {CameraView, cameraView, useCameraPermissions} from 'expo-camera'
+import { useState, useRef } from "react";
+import { View, StyleSheet, Image, Pressable } from "react-native";
+import { CameraView, useCameraPermissions } from "expo-camera";
+
 
 export default function Camera() {
-    const [permissao, pedirPermissao] = useCameraPermissions()
-    const [foto, setfoto] = useState(null)
-    const cameraRef = useRef(null)
-    if(!permissao){
-        return(
-            <View></View>
-        )
+    const [permissao, pedirPermissao] = useCameraPermissions();
+    const [foto, setFoto] = useState(null);
+    const [cameraType, setCameraType] = useState("back");
+    const cameraRef = useRef(null);
+
+    if (!permissao) {
+        return <View></View>;
     }
 
-    if(!permissao.granted) {
-        return(
+    if (!permissao.granted) {
+        return (
             <View style={styles.container}>
-                <Text style={styles.textopermissao}>Voce precisa dar permissao para utilizar a camera
-                </Text>
-                <Button title='pedir permissao' onPress={pedirPermissao} ></Button>
+                <Text style={styles.textpermission}>Você precisa autorizar o aplicativo para utilizar a câmera</Text>
+                <Button title="Dar Permissão" onPress={pedirPermissao} />
             </View>
-        )
+        );
     }
+
     const tirarFoto = async () => {
-        const foto = await camertaRef.current?.takePictureAsync({
+        const foto = await cameraRef.current?.takePictureAsync({
             quality: 0.5,
-            base64: true,
+            base64: true
+        });
+        setFoto(foto);
+    };
 
-        })
-        setfoto(foto)
-        console.log(foto)
-    }   
-    return(
-         <CameraView style={styles.camera} facing='front' ref={cameraRef} >
-            <View style={styles.container}>
-            <Button title='tirar foto' onPress={tirarFoto}></Button>
+    const alternarCamera = () => {
+        setCameraType((prevCameraType) => (prevCameraType === "back" ? "front" : "back"));
+    };
+
+    return (
+        <CameraView
+            facing={cameraType} 
+            style={styles.camera}
+            ref={cameraRef}
+        >
+            <View style={styles.buttonContainer}>
+                <Pressable onPress={alternarCamera} style={styles.switchButton}>
+                    <Image
+                        source={require('./alterna.png')}
+                        style={styles.switchImage}
+                    />
+                </Pressable>
+                <Pressable onPress={tirarFoto} style={styles.cameraButtonContainer}>
+                    <Image
+                        source={require('./botaaoo.png')}
+                        style={styles.cameraButton}
+                    />
+                </Pressable>
             </View>
-         </CameraView>
-    )
+        </CameraView>
+    );
 }
 
-const styles = StyleSheet.create ({
-    container:{
-       flex: 1,
-        justifyContent:'center',
-    },
-    textopermissao:{
-textAlign: 'Center',
-    },
-    camera:{
+const styles = StyleSheet.create({
+    container: {
         flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    textpermission: {
+        textAlign: 'center',
+        marginBottom: 20
+    },
+    camera: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    buttonContainer: {
+        flexDirection: 'row', 
+        width: '100%', 
+        alignItems: 'center',
+        justifyContent: 'space-between', 
+        paddingHorizontal: 20,
+        marginBottom: 20,
+    },
+    cameraButtonContainer: {
+        flex: 1, 
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    cameraButton: {
+        width: 70,
+        height: 70,
+        marginLeft: -50,
+        resizeMode: 'contain',
+    },
+    switchButton: {
+        justifyContent: 'flex-start',
+    },
+    switchImage: {
+        width: 50,
+        height: 50,
+        resizeMode: 'contain',
     }
-})
+});
